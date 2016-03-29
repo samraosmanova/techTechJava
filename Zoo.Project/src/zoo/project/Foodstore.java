e75aaae250d65cdf1266169751ccea322f244299
samraosmanova <samra.osmanova@gmail.com>
2016-03-29 22:34:28 +0400

    Added Foodstore class and initialized

--- Zoo.Project/src/zoo/project/Foodstore.java
+++ Zoo.Project/src/zoo/project/Foodstore.java
@@ -1,9 +1,33 @@
 package zoo.project;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
  * @author samra
  */
 public class Foodstore {
+
+    public Map<String,Integer> food;
+
+    public Foodstore() {
+        this.food = new HashMap();
+    }
     
+    /**
+     * 
+     * @param nameOfFood
+     * @param quantity 
+     */
+    public void addFood(String nameOfFood, int quantity){
+        food.put(nameOfFood, quantity);
+    }
+
+    /**
+     * 
+     * @param nameOfFood 
+     */
+    public void takeFood(String nameOfFood) {
+        food.replace(nameOfFood, food.get(nameOfFood) - 1);
+    }
 }
--- Zoo.Project/src/zoo/project/ZooProject.java
+++ Zoo.Project/src/zoo/project/ZooProject.java
@@ -4,8 +4,16 @@ package zoo.project;
  * @author samra
  */
 public class ZooProject {
+
     public static void main(String[] args) {
+        Foodstore foodstore = new Foodstore();
+        foodstore.addFood("hay", 5);
+        foodstore.addFood("steak", 10);
+        foodstore.addFood("fruit", 7);
+        foodstore.addFood("celery", 4);
+        foodstore.addFood("fish", 11);
+        foodstore.addFood("ice cream", 5);
         
+        System.out.println(foodstore.food);
     }
-    
 }
